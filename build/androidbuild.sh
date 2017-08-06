73ecaf1e4a2f09413e1d0f31ea02d0b0510df18f
showwaychen <459496617@qq.com>
2017-08-06 20:44:30 +0800

    use gun_stl for unique_ptr

--- build/androidbuild.sh
+++ build/androidbuild.sh
@@ -3,4 +3,4 @@ cmake ../ -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake \
           -DANDROID_ABI="armeabi-v7a" \
 		  -DANDROID_NATIVE_API_LEVEL=15 \
 		  -DANDROID_NDK=$NDKROOT \
-		  -DANDROID_STL=stlport_static
+		  -DANDROID_STL=gnustl_static
