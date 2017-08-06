51894e70a74245d35b82e7d63b34e64279a93078
showwaychen <459496617@qq.com>
2017-08-06 20:33:31 +0800

    use stlport

--- build/androidbuild.sh
+++ build/androidbuild.sh
@@ -1,2 +1,6 @@
 echo $NDKROOT
-cmake ../ -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake -DANDROID_ABI="armeabi-v7a" -DANDROID_NATIVE_API_LEVEL=15 -DANDROID_NDK=$NDKROOT
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake \
+          -DANDROID_ABI="armeabi-v7a" \
+		  -DANDROID_NATIVE_API_LEVEL=15 \
+		  -DANDROID_NDK=$NDKROOT \
+		  -DANDROID_STL=stlport_static
