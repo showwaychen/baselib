017e3d267ae12db2d2af96e0696cb031b866811e
chenxw001 <chenxw001@cmft.com>
2019-02-22 14:40:36 +0800

    添加对toolchain和stl的配置的支持。

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-
 echo $NDKROOT
 rm -R Build
 set -e
@@ -6,9 +5,13 @@ mkdir Build
 cd Build
 NTV=4.9
 STL=gnustl_static
-if [ "clang" = "$1" ];then
-NTV=clang
-STL=c++_static
+if [ "clang" = "$1" ]; then
+	NTV=clang
+	# STL=c++_static
+	STL=gnustl_static
+fi
+if [ x"gnu" = x"$2" ]; then
+	STL=gnustl_static
 fi
 echo "NTV=$NTV"
 cmake .. -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=$NTV -DCMAKE_ANDROID_NDK=$NDKROOT -DCMAKE_ANDROID_STL_TYPE=$STL -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=armeabi-v7a
